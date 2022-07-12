0ee17ff06ce2336155145a33ca839a914f940a80
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-07-12 17:37:12 -0300

    Primeiro commit, ainda contém erros  em resposta4.ts

---
+++ resposta1.ts
@@ -0,0 +1,6 @@
+// Uma forma de rodar isso em um arquivo .ts sem causar erros:
+
+let employee : {code: number, name: string} = {
+    code : 10,
+    name : "John"
+};
---
+++ resposta2.ts
@@ -0,0 +1,8 @@
+type pessoa = {nome: string, idade: number, profissao: string};
+
+
+let pessoa1 : pessoa = {
+    nome : "maria",
+    idade : 29,
+    profissao : "atriz"
+};
